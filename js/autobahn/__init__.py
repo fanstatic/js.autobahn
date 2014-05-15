a484504ab2a4fb91354d16b0989dc7927a4baa2b
Remco Wendt <remco.wendt@gmail.com>
2014-05-15 17:13:42 +0200

    Added autobahn

--- js/autobahn/__init__.py
+++ js/autobahn/__init__.py
@@ -2,6 +2,4 @@ from fanstatic import Library, Resource
 
 library = Library('autobahn|js', 'resources')
 
-# Define the resources in the library like this.
-# For options and examples, see the fanstatic documentation.
-# resource1 = Resource(library, 'style.css')
+autobahn = Resource(library, 'autobahn.js')
